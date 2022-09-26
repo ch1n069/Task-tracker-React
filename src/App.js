ca660d486ffbd29f5a61505c9bdfc7ea84a0ec71
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-26 13:47:43 +0300

    imorted the form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// components import
+
+import Form from "./components/form";
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Task tracker</h1>
       </header>
+      <Form />
     </div>
   );
 }
