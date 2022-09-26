df96d7a01c95fa8ac25fa651e0ba67d3a1be6232
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-26 15:15:48 +0300

    updated the application to display individual todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,16 @@
 import React from "react";
+// imports
 
-const TodoList = () => {
+import Todo from "./Todo";
+
+const TodoList = ({ todos }) => {
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo key={todo.id} text={todo.text} />
+        ))}
+      </ul>
     </div>
   );
 };
