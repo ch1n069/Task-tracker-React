7919d02af10238f7e3c575e1ce0234963573c4bf
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-26 14:21:24 +0300

    made an import of the use state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
 // components import
 
@@ -7,12 +7,15 @@ import Form from "./components/form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="App">
       <header>
         <h1>Task tracker</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
