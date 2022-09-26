b4855a9dc4539738a03fb26ffd4406a358b60296
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-26 13:43:14 +0300

    made an import of react into the component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Task tracker</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
